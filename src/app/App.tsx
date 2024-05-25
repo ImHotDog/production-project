aebc79757000955b29af8e2fe5733e728e2a246d
ImHotDog <hotabch-robert@mail.ru>
2024-05-25 22:17:19 +0300

    FSD refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import { Counter } from "./components/Counter";
 import './styles/index.scss';
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
 import { Link } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Suspense, useContext, useState } from "react";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { classNames } from "shared/lib/classNames";
 
 
 const App = () => {
@@ -22,8 +19,8 @@ const App = () => {
             <Link  to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
             
